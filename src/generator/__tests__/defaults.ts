c2bba2353c56c3aaf474f2c12d91d5ae03b312b8
Ian Hunter <ianhunter@gmail.com>
2021-02-16 16:33:12 -0600

    smoke test

--- src/generator/__tests__/defaults.ts
+++ src/generator/__tests__/defaults.ts
@@ -10,5 +10,11 @@ describe(".schema", () => {
   test("should set default Query", () => {
     const { schema, typeDefs } = subject(config)
     expect(schema).toBeDefined()
+    expect(typeDefs).toContain(`type Query {
+  Ping: String
+}
+type Mutation {
+  Ping: String
+}`)
   })
 })
