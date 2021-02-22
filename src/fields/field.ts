f83f3e9e00b1927253becf7c47e0ebaaae84f19d
Ian Hunter <ianhunter@gmail.com>
2021-02-22 09:45:41 -0600

    reorganizing fields, adding base field tests

--- src/fields/field.ts
+++ src/fields/field.ts
@@ -1,11 +1,11 @@
 export default class Field {
-  _gqlName: string
+  _name: string
   _required: boolean = false
   _params: object
   _guard: string = null
 
   constructor(name: string) {
-    this._gqlName = name
+    this._name = name
   }
 
   required(r = true): Field {
@@ -16,5 +16,13 @@ export default class Field {
   guard(scope): Field {
     this._guard = scope
     return this
+  }
+
+  toGQL(): string {
+    const fieldDef = [this._name]
+    if (this._required) fieldDef.push("!")
+    if (this._guard) fieldDef.push(` @${this._guard}`)
+
+    return fieldDef.join("")
   }
 }
--- src/fields/index.ts
+++ src/fields/index.ts
@@ -4,7 +4,9 @@ import Field from "./field"
 import Geo from "./geo"
 import PhoneNumber from "./phoneNumber"
 import String from "./string"
+
 import Type from "./type"
+import Array from "./array"
 
 export type GeneratedField = () => Field
 
@@ -14,6 +16,7 @@ export const geo: GeneratedField = () => new Geo()
 export const phoneNumber: GeneratedField = () => new PhoneNumber()
 export const string: GeneratedField = () => new String()
 export const type = (model) => new Type(model)
+export const array = (contains) => new Array(contains)
 
 export default {
   uuid,
@@ -22,4 +25,5 @@ export default {
   phoneNumber,
   string,
   type,
+  array,
 }
