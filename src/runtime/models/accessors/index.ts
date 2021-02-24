0d420fb1d499977e0a76afda0fd63685ed674db4
Ian Hunter <ianhunter@gmail.com>
2021-02-24 06:33:20 -0600

    refactoring and cleaning up list generation

--- src/runtime/models/accessors/index.ts
+++ src/runtime/models/accessors/index.ts
@@ -1,9 +1,7 @@
 import { Reducer } from "~/reducer"
 import { Fields, Accessors } from "~/types"
 import { generateFind } from "./find"
-
-export * from "./find"
-export * from "./list"
+import { generateList } from "./list"
 
 export function generateAccessors(label, accessors: Accessors, fields: Fields) {
   const reducer = new Reducer()
@@ -12,29 +10,10 @@ export function generateAccessors(label, accessors: Accessors, fields: Fields) {
     if (accessors.find) {
       reducer.reduce(generateFind(label, accessors.find /*fields*/))
     }
+    if (accessors.list) {
+      reducer.reduce(generateList(label, accessors.list /*fields*/))
+    }
   }
-
-  // const inputs = {}
-  // const mutationTypes = {}
-  // const mutationResolvers = {}
-
-  // const generatedFields = generateFields(fields, { guards: false })
-
-  // if (accessors) {
-  //   inputs[`${label}Input`] = generatedFields
-  // }
-
-  // return {
-  //   schema: {
-  //     inputs,
-  //     types: {
-  //       Mutation: mutationTypes,
-  //     },
-  //   },
-  //   resolvers: {
-  //     Mutation: mutationResolvers,
-  //   },
-  // }
 
   return reducer.toReducible()
 }
