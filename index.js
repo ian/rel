97c4c01fe3d197e20fdeb84a868093eb246764b2
Alisson <thelinuxlich@gmail.com>
2021-11-30 22:10:44 -0300

    now generating the schema file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { ApolloServer } from 'apollo-server-express'
 import { buildGraphbackAPI } from 'graphback'
 import { createRedisGraphProvider } from 'runtime-redisgraph'
 import expressPlayground from 'graphql-playground-middleware-express'
+import { SchemaCRUDPlugin } from '@graphback/codegen-schema'
 
 const app = express()
 app.get('/playground', expressPlayground.default({ endpoint: '/graphql' }))
@@ -23,6 +24,11 @@ const dataProviderCreator = createRedisGraphProvider()
 
 const { typeDefs, resolvers, contextCreator } = buildGraphbackAPI(schema, {
   dataProviderCreator,
+  plugins: [
+    new SchemaCRUDPlugin({
+      outputPath: './schema/schema.graphql',
+    }),
+  ],
 })
 
 const server = new ApolloServer({
