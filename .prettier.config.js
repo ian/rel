16751766a58396306821aa39af8cdd1f32a5891c
Alisson <thelinuxlich@gmail.com>
2021-11-30 22:05:54 -0300

    all good

---
+++ .prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "trailingComma": "es5",
+  "tabWidth": 4,
+  "semi": false,
+  "printWidth": 79,
+  "singleQuote": true
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import http from 'http'
+import { ApolloServer } from 'apollo-server-express'
+import { buildGraphbackAPI } from 'graphback'
+import { createRedisGraphProvider } from 'runtime-redisgraph'
+import expressPlayground from 'graphql-playground-middleware-express'
+
+const app = express()
+app.get('/playground', expressPlayground.default({ endpoint: '/graphql' }))
+
+const schema = `
+"""@model"""
+type Note {
+  id: ID!
+  text: String!
+  archived: Boolean!
+} 
+`
+
+// make sure you have redis running on localhost:6379 or change process.env.REDIS_HOST and process.env.REDIS_PORT
+
+const dataProviderCreator = createRedisGraphProvider()
+
+const { typeDefs, resolvers, contextCreator } = buildGraphbackAPI(schema, {
+  dataProviderCreator,
+})
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: [resolvers],
+  context: contextCreator,
+})
+
+await server.start()
+
+const httpServer = http.createServer(app)
+server.applyMiddleware({ app })
+
+await httpServer.listen({ port: 4000 }, () => {
+  console.log('Apollo Server on http://localhost:4000/graphql')
+})
