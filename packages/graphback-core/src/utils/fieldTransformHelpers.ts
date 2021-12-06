1877ea203b033bbb383bee1bbb5cb4a2b765f071
Alisson <thelinuxlich@gmail.com>
2021-12-06 11:57:49 -0300

    update graphql dependency on codegen-schema

--- packages/graphback-core/src/utils/fieldTransformHelpers.ts
+++ packages/graphback-core/src/utils/fieldTransformHelpers.ts
@@ -1,53 +1,54 @@
-import { parseMetadata } from "graphql-metadata";
-import { GraphQLObjectType } from 'graphql';
+import { parseMetadata } from 'graphql-metadata';
+import { GraphQLField, GraphQLObjectType } from 'graphql';
 
 export type FieldTransformer = (value?: any) => any;
 
-
 export interface FieldTransform {
-  fieldName: string,
-  transform: FieldTransformer
+  fieldName: string;
+  transform: FieldTransformer;
 }
 
 /* eslint-disable no-shadow */
 export enum TransformType {
-  UPDATE = "onUpdateFieldTransform",
-  CREATE = "onCreateFieldTransform"
+  UPDATE = 'onUpdateFieldTransform',
+  CREATE = 'onCreateFieldTransform',
 }
 /* eslint-enable no-shadow */
 
 export type FieldTransformMap = {
-  [TransformType.CREATE]: FieldTransform[],
-  [TransformType.UPDATE]: FieldTransform[]
+  [TransformType.CREATE]: FieldTransform[];
+  [TransformType.UPDATE]: FieldTransform[];
 };
 
-export function getFieldTransformations(baseType: GraphQLObjectType): FieldTransformMap {
+export function getFieldTransformations(
+  baseType: GraphQLObjectType,
+): FieldTransformMap {
   const fieldMap = baseType.getFields();
-  const fieldTransformMap : FieldTransformMap = {
+  const fieldTransformMap: FieldTransformMap = {
     [TransformType.CREATE]: [],
-    [TransformType.UPDATE]: []
+    [TransformType.UPDATE]: [],
   };
   for (const field of Object.values(fieldMap)) {
-    if (parseMetadata("updatedAt", field.description)) {
+    if (parseMetadata('updatedAt', field.description)) {
       fieldTransformMap[TransformType.UPDATE].push({
         fieldName: field.name,
         transform: () => {
           return new Date().getTime();
-        }
+        },
       });
       fieldTransformMap[TransformType.CREATE].push({
         fieldName: field.name,
         transform: () => {
           return new Date().getTime();
-        }
+        },
       });
     }
-    if (parseMetadata("createdAt", field.description)) {
+    if (parseMetadata('createdAt', field.description)) {
       fieldTransformMap[TransformType.CREATE].push({
         fieldName: field.name,
         transform: () => {
           return new Date().getTime();
-        }
+        },
       });
     }
   }
