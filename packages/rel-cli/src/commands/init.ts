0bd78662e07a4830f26eea1ca2198f7f33f7b0fd
Ian Hunter <ian@users.noreply.github.com>
2022-02-21 17:35:04 -0600

    CLI working great

--- packages/rel-cli/src/commands/init.ts
+++ packages/rel-cli/src/commands/init.ts
@@ -1,6 +1,12 @@
+import fs from "fs"
 import fsUtils from "fs-utils"
+import slugify from "slugify"
+import { execSync } from "child_process"
 import chalk from "chalk"
 import ora from "ora"
+import inquirer from "inquirer"
+
+const { version } = require("../../package.json")
 
 export default async function InitCommand() {
   console.log(`
@@ -14,32 +20,116 @@ export default async function InitCommand() {
 
   const dir = process.cwd()
 
-  const projectDir = `${dir}/rel`
-  const initializing = ora(`Initializing Rel`).start()
+  // default installation path
+  // let projectDir = `${dir}/rel`
+
+  const isNPM = fs.existsSync("./package-lock.json")
+  const isYarn = fs.existsSync("./yarn.lock")
+  const isPNPM = fs.existsSync("./pnpm-lock-yaml")
+  const isExistingProject = isNPM || isYarn || isPNPM
+
+  let cmd = "npm"
+  let projectDir = "."
+
+  if (isExistingProject) {
+    console.log("Exising project found in .")
+
+    switch (true) {
+      case isNPM:
+        cmd = "npm"
+        break
+      case isYarn:
+        cmd = "yarn"
+        break
+      case isPNPM:
+        cmd = "pnpm"
+        break
+    }
+  } else {
+    const { projectName } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "What's the name of your project?",
+        default: "api",
+      },
+    ])
+
+    const projectSlug = slugify(projectName, {
+      remove: /[!@#$%^&*()_+|}{:"?><\[\];',./}]/g,
+    })
+    projectDir = `${dir}/${projectSlug}`
+    fs.mkdirSync(projectDir)
+    process.chdir(projectDir)
+
+    await fsUtils.writeFileSync(
+      `./package.json`,
+      `
+{
+"name": "${projectName}",
+"version": "0.0.1",
+"description": "",
+"main": "index.js",
+"license": "MIT",
+"scripts": {
+  "dev": "rel dev",
+  "redis": "docker run -p 6379:6379 redislabs/redismod"
+},
+"devDependencies": {},
+"dependencies": {}
+}`
+    )
+  }
+
+  const initializing = ora(`Installing Rel packages`).start()
+  execSync(`${cmd} add rel-cmd@${version}`)
+  initializing.succeed(`Rel packages installed`)
+
+  const { path } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "path",
+      message: "Base directory for Rel project?",
+      default: isExistingProject ? "./rel" : ".",
+    },
+  ])
 
   await fsUtils.writeFileSync(
-    `${projectDir}/schema.graphql`,
+    `${path}/rel.config.js`,
     `
-type Post {
-  id: ID!
-  body: String!
-  owner: User!
+const relConfig = {
+  baseDir: "${path}"
 }
-
-type User {
-  id: ID!
-  name: String!
-  post: [Post]
-}
+    
+module.exports = relConfig
 `
   )
 
-  initializing.succeed(
-    "Your Rel schema was created at " + chalk.greenBright("rel/schema.graphql")
+  await fsUtils.writeFileSync(
+    `${path}/schema.graphql`,
+    `
+type Movie {
+  title: String!
+  year: Int
+  rating: Float
+  genres: [Genre]! @rel(label: "IN_GENRE", direction: OUT)
+}
+
+type Genre {
+  name: String
+  movies: [Movie]! @rel(label: "IN_GENRE", direction: IN)
+}
+    
+`
   )
 
   console.log()
-  console.log("Next steps:")
+  console.log()
+  console.log(
+    `Success! Created reljs at directory`,
+    chalk.greenBright(projectDir)
+  )
+  console.log(`Next steps:`)
   console.log()
   console.log(
     "1. Run " +
@@ -48,9 +138,16 @@ type User {
   )
   console.log(
     "2. Edit " +
-      chalk.greenBright("rel/schema.graphql") +
+      chalk.greenBright(`${path}/schema.graphql`) +
       " to update your schema."
   )
   console.log("3. Read more documentation at https://rel.run/docs")
   console.log()
+}
+
+type Question = {
+  type: string
+  name: string
+  message: string
+  default?: string
 }
